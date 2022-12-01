01f205b59514db927e6d7f33b3bd0ada8f7f3975
MeerKat <98255504+MeerKatPT@users.noreply.github.com>
2022-12-01 15:12:02 +0000

    2nd deploy

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { restart } = require("nodemon");
 const router = express.Router();
 const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
